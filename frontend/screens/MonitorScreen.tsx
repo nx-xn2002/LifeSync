659fe3b3253bfa114f7092b056e0b1b66aafb965
nx-xn2002 <1870230468@qq.com>
2025-01-30 09:21:46 +0800

    ✨ Get camera ready

--- frontend/screens/MonitorScreen.tsx
+++ frontend/screens/MonitorScreen.tsx
@@ -1,44 +1,32 @@
-import {StatusBar, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Dimensions, StatusBar, StyleSheet, Text, View} from "react-native";
 import type {BottomTabNavigationHelpers} from "@react-navigation/bottom-tabs/src/types";
 import {Button, ButtonText} from "@/components/ui";
 import {useState} from "react";
 import {CameraType, CameraView, useCameraPermissions} from "expo-camera";
 
 export default function MonitorScreen({navigation}: { navigation: BottomTabNavigationHelpers }) {
-    const [facing, setFacing] = useState<CameraType>('back');
     const [permission, requestPermission] = useCameraPermissions();
     if (!permission) {
-        // Camera permissions are still loading.
         return <View/>;
-    }
-    if (!permission.granted) {
-        // Camera permissions are not granted yet.
-        return (
-            <View style={styles.container}>
-                <Text style={styles.message}>We need your permission to show the camera</Text>
-                <Button onPress={requestPermission}>
-                    <ButtonText>
-                        Grant Permission
-                    </ButtonText>
-                </Button>
-            </View>
-        );
-    }
-
-    function toggleCameraFacing() {
-        setFacing(current => (current === 'back' ? 'front' : 'back'));
     }
 
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Monitor Screen</Text>
-            <CameraView style={styles.camera} facing={facing}>
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-                        <Text style={styles.text}>Flip Camera</Text>
-                    </TouchableOpacity>
-                </View>
-            </CameraView>
+            <View style={{
+                backgroundColor: 'black',
+                width: Dimensions.get('window').width * 0.5,
+                height: Dimensions.get('window').width * 0.5,
+                borderRadius: Dimensions.get('window').width * 0.5,
+            }}>
+                <CameraView style={styles.camera} facing={'back'}>
+                </CameraView>
+            </View>
+            <Button onPress={requestPermission}>
+                <ButtonText>
+                    Start Camera
+                </ButtonText>
+            </Button>
         </View>
     );
 }
@@ -52,16 +40,8 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         marginBottom: 20,
-    }, message: {
-        textAlign: 'center',
-        paddingBottom: 10,
     }, camera: {
-        flex: 1,
-    }, buttonContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        backgroundColor: 'transparent',
-        margin: 64,
+        flex: 1
     },
     button: {
         flex: 1,
